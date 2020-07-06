590b195c451cabddc768d83ed031447888aeae58
Anna Nicole Reed <reedanna@mail.com>
2020-07-06 20:20:57 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,7 +5,7 @@ def apply_coupons(cart, coupons)
   #
   # REMEMBER: This method **should** update cart
   cart.each |grocery| do
-    current_item = 
+    current_item = find_item_by_name_in_collection(grocery, coupons)
   end
 end
 

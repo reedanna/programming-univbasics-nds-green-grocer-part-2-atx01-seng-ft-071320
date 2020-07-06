eea5ff595b6fe2667896c8a5ccafc0288f1fc235
Anna Nicole Reed <reedanna@mail.com>
2020-07-06 20:46:58 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -44,8 +44,16 @@ end
 
 apply_coupons(
   [
-  {:item => "AVOCADO", :price => 3.00, :clearance => true, :count => 3},
-  {:item => "KALE",    :price => 3.00, :clearance => false, :count => 1}
+      {:item => "AVOCADO", :price => 3.00, :clearance => true},
+      {:item => "KALE", :price => 3.00, :clearance => false},
+      {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+      {:item => "ALMONDS", :price => 9.00, :clearance => false},
+      {:item => "TEMPEH", :price => 3.00, :clearance => true},
+      {:item => "CHEESE", :price => 6.50, :clearance => false},
+      {:item => "BEER", :price => 13.00, :clearance => false},
+      {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
+      {:item => "BEETS", :price => 2.50, :clearance => false},
+      {:item => "SOY MILK", :price => 4.50, :clearance => true}
   ],
   [
   {:item => "AVOCADO", :num => 2, :cost => 5.00}
